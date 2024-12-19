8b9bb9ab73d1c959c59b69b6ad82c1b362f4e6bd
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2024-12-19 15:27:21 +0300

    add routes.php

---
+++ configs/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+    $routes = array (
+        'UserController' => array(
+            'reg' => 'reg',
+            'auth' => 'auth',
+            'logout' => 'logout',
+            'account' => 'account',
+        ),
+        'MainController' => array(
+            '' => 'index'
+        )
+    );
+
