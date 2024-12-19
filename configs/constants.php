22a962f10ce54cdaaae4b2c17bc0e82d6ccd468b
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2024-12-19 17:02:54 +0300

    update constants.php. add views (header, footer). add bootstrap in libs

--- configs/constants.php
+++ configs/constants.php
@@ -2,3 +2,5 @@
 
     define("SITE_ROOT", "/only_test/");
     define("FULL_SITE_ROOT", "http://localhost" . SITE_ROOT);
+    define("ASSETS", FULL_SITE_ROOT . "assets/");
+    define("LIBS", ASSETS . "libs/");
