465e516a80b7e75daaf01c01253cb8060bd3429e
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2024-12-20 11:04:16 +0300

    add configs/db.php and components/db.php

---
+++ configs/db.php
@@ -0,0 +1,9 @@
+<?php
+
+    $db = array(
+        'HOST' => 'localhost',
+        'USER' => 'root',
+        'PASSWORD' => '',
+        'DB_NAME' => 'only_test',
+        'CHARSET' => 'utf8'
+    );
